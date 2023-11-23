755868fd0305b53b7d6f066d678a99534bb805bf
Lazar Zlatev <lazarzlatev81@gmail.com>
2023-11-23 11:31:19 +0300

    homework6

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
-from currency.views import rate_list
-from currency.views import contactus_list
+from currency.views import rate_list, contactus_list, status_code
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rate/list',rate_list),
-    path('contactus/list',contactus_list),
+    path('rate/list', rate_list),
+    path('contactus/list', contactus_list),
+    path('sc/', status_code),
 ]
