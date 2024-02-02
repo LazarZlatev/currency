a984f84193ee81116df07786bda1b9c6acb00f50
Lazar Zlatev <lazarzlatev81@gmail.com>
2024-02-02 21:08:51 +0300

    cache

--- app/currency/apps.py
+++ app/currency/apps.py
@@ -6,6 +6,9 @@ class CurrencyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'currency'
 
+    def ready(self):
+        import account.receivers # NOQA F401
+
 
 class SuitConfig(DjangoSuitConfig):
     layout = 'horizontal'
